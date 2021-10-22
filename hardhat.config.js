4816a8a2cbe950f400985888dcc1551bd0126cbc
Eric Nordelo <eric.nordelo39@gmail.com>
2021-10-22 18:09:50 -0400

    refactor: remove unnecesary dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,7 @@
 require('@nomiclabs/hardhat-ethers');
-require('@nomiclabs/hardhat-waffle');
-require('@nomiclabs/hardhat-etherscan');
-require('@nomiclabs/hardhat-solhint');
 require('@nomiclabs/hardhat-web3');
 require('@nomiclabs/hardhat-truffle5');
-require('@openzeppelin/hardhat-upgrades');
-require('solidity-coverage');
-require('mocha-skip-if');
-require('hardhat-gas-reporter');
-require('hardhat-contract-sizer');
 require('hardhat-deploy');
-require('hardhat-docgen');
 
 require('dotenv').config();
 
@@ -18,23 +9,11 @@ const MNEMONIC = process.env.MNEMONIC;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
-const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
 
 module.exports = {
-  docgen: {
-    path: './docs',
-    clear: true,
-    runOnCompile: false,
-  },
   networks: {
     hardhat: {
       tags: ['local'],
-    },
-    rinkeby_fork: {
-      url: 'http://127.0.0.1:7545', // ganache local network
-      forking: {
-        url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      },
     },
     localhost: {
       url: 'http://127.0.0.1:8545', // hardhat local network
@@ -53,35 +32,19 @@ module.exports = {
       accounts: { mnemonic: MNEMONIC },
       tags: ['testnet'],
     },
-    rinkeby_polygon: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: { mnemonic: MNEMONIC },
-      deploy: ['deploy/polygon'],
-      tags: ['testnet'],
-    },
     mumbai: {
       url: MUMBAI_RPC_URL,
       accounts: [PRIVATE_KEY],
-      deploy: ['deploy/polygon'],
       tags: ['testnet'],
     },
   },
   namedAccounts: {
     deployer: 0,
-    player: 1,
-    signatory: 2,
-  },
-  gasReporter: {
-    enabled: false,
   },
   mocha: {
     timeout: 999999,
   },
-  etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: POLYGONSCAN_API_KEY,
-  },
+
   solidity: {
     compilers: [
       {
@@ -92,15 +55,6 @@ module.exports = {
             runs: 200,
           },
         },
-      },
-      {
-        version: '0.6.6',
-      },
-      {
-        version: '0.4.17',
-      },
-      {
-        version: '0.5.16',
       },
     ],
   },
